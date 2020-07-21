865cc4e0392c4dd7c3f1b9151f10d28d9652919b
dragon3hgmx <dragon3h@gmx.com>
2020-07-21 11:21:22 -0400

    added production dockerfile

--- src/App.js
+++ src/App.js
@@ -15,8 +15,8 @@ function App() {
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
-        >
-          Learn React
+        > 
+          Hi Romr!
         </a>
       </header>
     </div>
